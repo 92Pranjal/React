50f26db9d11608719b93b29cccf1d051c01dd05a
92Pranjal <pranjalpriyadarshi01@gmail.com>
2021-12-07 11:57:43 +0530

    Lesson 4: With and without JSX

---
+++ lesson_4/src/App.js
@@ -0,0 +1,14 @@
+import MyComponent from './components/Greet';
+import Welcome from './components/Welcome'
+import Hello from './components/Hello'
+function App() {
+  return (
+    <div>
+      <Hello/>
+    {/* //   <MyComponent/>
+    //   <Welcome/> */}
+    </div>
+  );
+}
+
+export default App;
